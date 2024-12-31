e399b36240613ddafe6faa63ffae2ee0b84b9332
footGunDev <83507106+KNelTech@users.noreply.github.com>
2024-12-31 16:13:42 -0600

    first react next

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+    return (
+      <section>
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        <p>Feel free to reach out!</p>
+      </section>
+    )
+  }
